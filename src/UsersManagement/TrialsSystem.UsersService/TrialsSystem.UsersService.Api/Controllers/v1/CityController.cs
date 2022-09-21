f41ee828fb422e5f9599a7ad5477eefe6d4adaa3
artyom <artyom.karpets@softeq.com>
2022-09-21 18:32:18 +0600

    AddCityController and GenderController

---
+++ src/UsersManagement/TrialsSystem.UsersService/TrialsSystem.UsersService.Api/Controllers/v1/CityController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrialsSystem.UsersService.Api.Controllers.v1
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CityController : ControllerBase
+	{
+	}
+}
