521a98734abc217e6da019a75657a25ef31774e2
artyom <artyom.karpets@softeq.com>
2022-09-21 18:30:35 +0600

    Add UsersService

---
+++ src/UsersManagement/TrialsSystem.IdentityService/TrialsSystem.IdentityService.Infrastructure/Services/UsersGatewayService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrialsSystem.IdentityService.Infrastructure.Services
+{
+    public class UsersGatewayService
+    {
+    }
+}
